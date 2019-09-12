b329fa5e04a221f98855e24e8e599eebf5565070
Nikita Gerasimoski <Nikita.Gerasimoski@netcetera.com>
2019-09-12 08:01:02 +0200

    Initial project setup

---
+++ eScores/eScores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eScores
+//
+//  Created by Nikita Gerasimoski on 9/9/19.
+//  Copyright © 2019 Nikita Gerasimoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
