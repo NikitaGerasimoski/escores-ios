706a12cfb99935735ff4de441d2fe6aa8a9709df
Nikita Gerasimoski <Nikita.Gerasimoski@netcetera.com>
2019-09-15 21:34:34 +0200

    Controllers and utility classes initial setup

---
+++ eScores/eScores/Utilities/PandaScoreServices.swift
@@ -0,0 +1,9 @@
+//
+//  PandaScoreServices.swift
+//  eScores
+//
+//  Created by Nikita Gerasimoski on 9/15/19.
+//  Copyright © 2019 Nikita Gerasimoski. All rights reserved.
+//
+
+import Foundation
---
+++ eScores/eScores/ViewControllers/eScoresTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  eScoresTabBarController.swift
+//  eScores
+//
+//  Created by Nikita Gerasimoski on 9/15/19.
+//  Copyright © 2019 Nikita Gerasimoski. All rights reserved.
+//
+
+import UIKit
+
+class eScoresTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
